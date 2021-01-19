bd1ec86f89cd7d9b8c9fb34b7279dd24ce482965
radrigo97 <stitchofficial01@gmail.com>
2021-01-19 19:06:43 +0300

    Server-babel

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const PORT = 5000;
-
 
 app.get('/', home);
 app.get('/info', info);
 
-
-function home(req,res){
+function home(req, res) {
   res.send('Alex');
 }
 
-function info(req,res){
+function info(req, res) {
   const a = 5;
   const b = Math.random();
   const c = a + b;
   res.send('info' + c);
 }
 
-app.listen(PORT,()=> {
+app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });

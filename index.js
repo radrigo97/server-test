ed79002f87cc858c9f1482a77e5d853e25a6a637
radrigo97 <stitchofficial01@gmail.com>
2021-01-19 15:03:22 +0300

    Server-eslint

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const app = express();
 const PORT = 5000;
 
 
-app.get('/', home)
-app.get('/info', info)
+app.get('/', home);
+app.get('/info', info);
 
 
 function home(req,res){
-    res.send('Aladadadad')
+  res.send('Alex');
 }
 
 function info(req,res){
-    const a = 5;
-    const b = Math.random()
-    const c = a + b
-    res.send('info' + c)
+  const a = 5;
+  const b = Math.random();
+  const c = a + b;
+  res.send('info' + c);
 }
 
 app.listen(PORT,()=> {
-    console.log(`Example app listening at http://localhost:${PORT}`)
-})
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});

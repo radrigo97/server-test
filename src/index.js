17a98b91798fb4ead13cabe3624b750f8f5146b1
radrigo97 <stitchofficial01@gmail.com>
2021-01-20 12:46:36 +0300

    Server-json

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ const PORT = 5000;
 
 app.get('/', home);
 app.get('/info', info);
+app.use(apiNotFount);
 
 function home(req, res) {
-  res.send('Alex');
+  res.send('Alex----');
 }
 
 function info(req, res) {
@@ -14,6 +15,10 @@ function info(req, res) {
   const b = Math.random();
   const c = a + b;
   res.send('info' + c);
+}
+
+function apiNotFount(req, res) {
+  res.status(400).json('API not found');
 }
 
 app.listen(PORT, () => {

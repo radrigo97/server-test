ff03c7d0ed41aabcd528dad54c5aaf09a2ae9f1c
radrigo97 <stitchofficial01@gmail.com>
2021-01-20 13:08:17 +0300

    Server-import-export

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import express from 'express';
+import home from './home';
+import info from "./info";
+
 const app = express();
 const PORT = 5000;
 
 app.get('/', home);
 app.get('/info', info);
 app.use(apiNotFount);
-
-function home(req, res) {
-  res.send('Alex----');
-}
-
-function info(req, res) {
-  const a = 5;
-  const b = Math.random();
-  const c = a + b;
-  res.send('info' + c);
-}
 
 function apiNotFount(req, res) {
   res.status(400).json('API not found');

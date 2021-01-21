7bffb24eaa2c1dc7e61884bc9e1b6e1ebdf8491b
radrigo97 <stitchofficial01@gmail.com>
2021-01-21 09:12:02 +0300

    Server-body-parser

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import home from './home';
-import info from "./info";
+import info from './info';
+import errorHandler from './arrowHandler';
+import logger from './logger';
+import parseResponse from './parseResponse';
 
 const app = express();
 const PORT = 5000;
 
+logger(app);
+parseResponse(app);
 app.get('/', home);
-app.get('/info', info);
-app.use(apiNotFount);
-
-function apiNotFount(req, res) {
-  res.status(400).json('API not found');
-}
+app.post('/info', info);
+errorHandler(app);
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
